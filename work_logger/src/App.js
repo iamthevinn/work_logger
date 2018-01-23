e5ea3e15b83c5f51c9d62d033b0121e605583974
Vince Carini <vincecarini@northwesternmutual.com>
2018-01-23 13:03:57 -0600

    ok. now i think i got the picture

--- work_logger/src/App.js
+++ work_logger/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 
 const Card = (props) => {
-  
+
 }
 
 const App = () => {
@@ -39,6 +39,44 @@ const App = () => {
             </div>
           </form>
         </div>
+        <div style = {{height: "60vh", borderTop: "1px solid black", display: "flex", alignItems: "center", justifyContent: "center"}}>
+        <div style = {{height: "300px", width: "300px", border: "1px solid black", display:"inline-block", margin: "10px"}}>
+          <div style = {{height: "60px", display:"flex"}}>
+            <div style = {{width: "50%", textAlign:"left", padding: "5px", fontSize: "24px"}}>
+              Personal
+            </div>
+            <div style = {{width: "50%", textAlign:"right", padding: "5px", fontSize: "24px"}}>
+              0:30
+          </div>
+          </div>
+          <div style = {{height: "240px", paddingLeft: "5px"}}>
+            <div style = {{width: "100%", display: "flex"}}>
+              <div style={{paddingRight:"5px"}}>0:30</div><div>Learn React</div>
+            </div>
+          </div>
+        </div>
+        <div style = {{height: "300px", width: "300px", border: "1px solid black", display:"inline-block", margin: "10px"}}>
+          <div style = {{height: "60px", display:"flex"}}>
+            <div style = {{width: "50%", textAlign:"left", padding: "5px", fontSize: "24px"}}>
+              Work
+            </div>
+            <div style = {{width: "50%", textAlign:"right", padding: "5px", fontSize: "24px"}}>
+              4:00
+          </div>
+          </div>
+          <div style = {{height: "240px", paddingLeft: "5px"}}>
+            <div style = {{width: "100%", display: "flex"}}>
+              <div style={{paddingRight:"5px"}}>2:30</div><div>Sprint planning</div>
+            </div>
+            <div style = {{width: "100%", display: "flex"}}>
+            <div style={{paddingRight:"5px"}}>1:30</div><div>Usability testing</div>
+          </div>
+          <div style = {{width: "100%", display: "flex"}}>
+          <div style={{paddingRight:"5px"}}>1:30</div><div>Another Line</div>
+        </div>
+          </div>
+        </div>
+     </div>
       </div>
     );
   }
---
+++ work_logger/src/notes.md
@@ -0,0 +1,5 @@
+# work logger
+
+- Need to have state in projectType, description, minutes, workItems [], personalItems []
+- card object that doesn't need state
+- 
