691252abf2226db038251d6c81c99f294d3ff9bd
Vince Carini <vincecarini@northwesternmutual.com>
2018-01-23 12:35:55 -0600

    stateless created

--- work_logger/src/App.js
+++ work_logger/src/App.js
@@ -2,20 +2,61 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+
+const Card = (props) => {
+  
+}
+
+const App = () => {
+  //render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style = {{height: "100vh"}}>
+        <div className="header">
+          <h1>Welcome to React</h1>
+        </div>
+        <div style = {{height: "40vh"}}>
+          <form>
+            <div>
+              <label>Project </label>
+              <select>
+                <option value = "personal">Personal</option>
+                <option value = "work">Work</option>
+              </select>
+            </div>
+            <br />
+            <div>
+              <label>Description </label>
+              <input type="text" placeholder="At least 5 characters"></input>
+            </div>
+            <br />
+            <div>
+              <label>Minutes </label>
+              <input type="number" placeholder="Between 0 and 240"></input>
+            </div>
+            <br />
+            <div>
+              <button>Add</button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
-}
-
+//}
+/*
+        <div style = {{height: "60vh", borderTop: "1px solid black", display: "flex", alignItems: "center", justifyContent: "center"}}>
+           <div style = {{height: "150px", width: "150px", border: "1px solid black", display:"inline-block", margin: "10px"}}>
+             stuff
+           </div>
+           <div style = {{height: "150px", width: "150px", border: "1px solid black", display:"inline-block", margin: "10px"}}>
+            stuff
+          </div>
+         <div style = {{height: "150px", width: "150px", border: "1px solid black", display:"inline-block", margin: "10px"}}>
+         stuff
+       </div>
+                  <div style = {{height: "150px", width: "150px", border: "1px solid black", display:"inline-block", margin: "10px"}}>
+           stuff
+         </div>
+        </div>
+*/
 export default App;
